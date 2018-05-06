c22c3ddcd3d219ab2eac03a2ee55311513872e7c
An Le <an.le@cureus.com>
2018-05-06 16:19:52 -0700

    config shit

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Devweek16
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
