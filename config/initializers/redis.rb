6635e46fcdcb785d7da7e5ccbc3ba5ee2a3bab1a
An Le <an.le@cureus.com>
2018-05-06 16:33:49 -0700

    redis initialize

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,10 @@
 
 if ENV["REDISCLOUD_URL"]
     $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  $redis = Redis.new(:host => 'localhost', :port => 6379)
 end
 
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+
 
 # $redis = Redis.new(:url => "redis://rediscloud:devweeksuxs@pub-redis-17785.us-west-2-1.1.ec2.garantiadata.com:17785")
